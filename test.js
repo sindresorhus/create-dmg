1f68d20be6f414e6b556b74f69e992a2f9ef37cb
Sindre Sorhus <sindresorhus@gmail.com>
2017-03-31 19:52:13 +0700

    Correctly silence code signing failure in the tests

--- test.js
+++ test.js
@@ -7,6 +7,15 @@ import tempfile from 'tempfile';
 test(async t => {
 	const cwd = tempfile();
 	fs.mkdirSync(cwd);
-	await t.throws(execa(path.join(__dirname, 'cli.js'), [path.join(__dirname, 'fixture.app')], {cwd}), /Code signing failed/);
+
+	try {
+		await execa(path.join(__dirname, 'cli.js'), [path.join(__dirname, 'fixture.app')], {cwd});
+	} catch (err) {
+		// Silence code signing failure
+		if (!/Code signing failed/.test(err.message)) {
+			throw err;
+		}
+	}
+
 	t.true(fs.existsSync(path.join(cwd, 'fixture-0.0.1.dmg')));
 });
