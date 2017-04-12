2f4dc8d6afb827eae6c866959b6095ee6c790914
Gilles De Mey <gilles.de.mey@gmail.com>
2017-04-12 17:40:01 +0200

    adds a simple unit test

--- test.js
+++ test.js
@@ -19,3 +19,21 @@ test(async t => {
 
 	t.true(fs.existsSync(path.join(cwd, 'fixture-0.0.1.dmg')));
 });
+
+test('DMG format option', async t => {
+	const cwd = tempfile();
+	fs.mkdirSync(cwd);
+
+	try {
+		await execa(path.join(__dirname, 'cli.js'), ['-f', 'UDZO', path.join(__dirname, 'fixture.app')], {cwd});
+	} catch (err) {
+		// Silence code signing failure
+		if (!/Code signing failed/.test(err.message)) {
+			throw err;
+		}
+	}
+
+	const fixture = path.join(cwd, 'fixture-0.0.1.dmg');
+	const {stdout} = await execa('hdiutil', ['imageinfo', fixture], {cwd});
+	t.true(stdout.includes('Format: UDZO'));
+});
