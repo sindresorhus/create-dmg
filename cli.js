de8006f3d8590be9f8b9f42e5317ca8ac938d906
Sindre Sorhus <sindresorhus@gmail.com>
2018-11-18 17:37:27 +0700

    Update cli.js

--- cli.js
+++ cli.js
@@ -19,7 +19,7 @@ const cli = meow(`
 
 	Options
 	  --overwrite         Overwrite existing DMG with the same name
-	  --identity=<value>  Set code signing identity (chosen automatically by default)
+	  --identity=<value>  Manually set code signing identity (automatic by default)
 
 	Examples
 	  $ create-dmg 'Lungo.app'
