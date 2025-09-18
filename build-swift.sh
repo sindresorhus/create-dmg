5f15a15771b1471b4b5770edda98dc8ca573104d
Guillaume Sabran <sabranguillaume@gmail.com>
2025-09-18 12:06:50 -0700

    fix tests

--- build-swift.sh
+++ build-swift.sh
@@ -7,7 +7,6 @@ cd imageComposition
 xcrun swift build -c release --arch arm64 --arch x86_64 --product compose-icon
 
 # Copy the built executable to the root directory for easy access
-BIN_DIR="$(xcrun swift build -c release --show-bin-path)"
-cp "$BIN_DIR/compose-icon" ../compose-icon
+cp ".build/apple/Products/Release/compose-icon" ../compose-icon
 
 echo "Swift executable built successfully: compose-icon"
